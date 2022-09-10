6b398b6afc86a38fed04c99c5621cbbf415d5f1f
Martian66 <kimmartin049@gmail.com>
2022-09-10 04:05:08 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -  prints all possible combinations of single-digit numbers
+ * Description: using the main function
+ * Return: Always 0
+ */
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+	putchar(c);
+
+if (c != 57)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
