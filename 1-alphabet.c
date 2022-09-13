bf3bbf0c2d84e5067b3dfc2dab1530d160807ef2
Martian66 <kimmartin049@gmail.com>
2022-09-13 11:09:56 -0700

    1-alphabet.c

---
+++ 1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints in lowercase
+ */
+
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+}
