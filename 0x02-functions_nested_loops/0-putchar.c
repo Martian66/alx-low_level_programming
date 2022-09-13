54ec42b3ce094204e54549565c300932d5c52539
Martian66 <kimmartin049@gmail.com>
2022-09-13 10:07:18 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  prints _putchar
+ * Description: using the main function
+ * Return: Always 0
+ */
+int main(void)
+{
+char *km = "_putchar"
+
+while (*km)
+{
+_putchar(*km);
+km++;
+}
+_putchar('\n');
+
+return (0);
+}
