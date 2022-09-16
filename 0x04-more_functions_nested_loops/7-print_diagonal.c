1edf9953e69738572831b8fe7df7b24daf666cca
Martian66 <kimmartin049@gmail.com>
2022-09-16 06:34:47 -0700

    7-print_diagonal..c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_diagonal - prints a diagonal
- * @n: parameter
+ *print_diagonal - prints a diagonal
+ *@n: parameter
  */
+
 void print_diagonal(int n)
 {
 int len, space;
 
 if (n > 0)
 {
-for (len = 0; space < len; space++)
+for (len = 0; len < n; len++)
+{
+for (space = 0; space < len; space++)
 {
 putchar (' ');
 }
