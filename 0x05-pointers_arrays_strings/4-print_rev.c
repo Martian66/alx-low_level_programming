678c0bcc33b1e564e957d9fd1dac1ff35a2cd87b
Martian66 <kimmartin049@gmail.com>
2022-09-19 08:07:53 -0700

    4-print_rev..c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * print_rev - prints a string in reverse
- * @s: string to reverse
- * Return: no value
+ *print_rev - prints a string in reverse
+ *@s: string to reverse
+ *Return: no value
  */
+
 void print_rev(char *s)
 {
+
 int len = strlens(s);
 
 while (len--)
