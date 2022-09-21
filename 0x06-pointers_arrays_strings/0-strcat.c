13a04946fc58047c28b6fbd20dff2be1d3549f5d
Martian66 <kimmartin049@gmail.com>
2022-09-21 08:50:46 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcat - a function that concatenates two strings.
+ *@src: string to be appended
+ *@dest: String to be used
+ *
+ *Return: A pointer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+size_t size = strlen(dest);
+
+for (size_t i = 0; i <= strlen(source); i++)
+
+dest[size + i] = source[i];
+
+return (dest);
+
+}
