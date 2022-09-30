8c06f7905e6e28815a3ba3f006c7ee69ba08d3e9
Martian66 <kimmartin049@gmail.com>
2022-09-30 11:54:51 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  a program that adds positive numbers.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 if a non-integer is among the passed in arguments, else 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+int total, i;
+char *p;
+int num;
+
+total = 0;
+if (argc > 1)
+{
+
+for (i = 1; argv[i]; i++)
+
+{
+num = strtol(argv[i], &p, 10);
+if (!*p)
+
+total += num;
+
+else
+
+{
+
+printf("Error\n");
+
+return (1);
+
+}
+
+}
+
+}
+
+printf("%d\n", total);
+
+}
