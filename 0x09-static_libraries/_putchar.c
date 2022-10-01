d6d2be9fe4e80de0bed2407df1324794f55d7527
Martian66 <kimmartin049@gmail.com>
2022-10-01 00:51:46 -0700

    libmy.a

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1 -1 if error
+ */
+
+int _putchar(char c)
+
+{
+
+return (write(1, &c, 1));
+
+}
