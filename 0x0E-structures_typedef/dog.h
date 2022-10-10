d2aae538368a576212c112744f65b7e4ba4bdee7
Martian66 <kimmartin049@gmail.com>
2022-10-10 10:07:04 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,17 @@
 #ifndef _DOG_H_
 #define _DOG_H
 
-int _putchar(char c);
+struct dog
+{
 
+char *name;
+float age;
+char *owner;
+
+};
+
+typedef struct dog dog_t;
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
