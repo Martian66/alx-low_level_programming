fbe4c10a055c3064d0b9c85cb1d65e94ed5170c7
Martian66 <kimmartin049@gmail.com>
2022-10-11 01:13:54 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,44 @@
 #include <stdlib.h>
 
 /**
+ *_strdup - returns a pointer to a newly allocated space in memory,
+ *which contains a copy of the string given as a parameter
+ *@str: The string to copy
+ *
+ *Return: a pointer to the duplicated string or NULL if not enough memory
+ */
+
+char *_strdup(char *str)
+{
+char *ar;
+unsigned int i = 0;
+unsigned int k = 0;
+
+if (str == NULL)
+return (NULL);
+
+while (str[i])
+i++;
+ar = malloc(sizeof(char) * (i + 1));
+
+if (ar == NULL)
+return (NULL);
+
+while (str[k])
+{
+ar[k] = str[k];
+k++;
+
+}
+
+ar[k] = 0;
+return (ar);
+
+}
+
+
+
+/**
  *new_dog - a function that creates a new dog
  *@name: name of dog
  *@age: age of dog
@@ -13,33 +51,33 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
-dog_t *p;
+dog_t *new;
+char *ncpy;
+char *ocpy;
 
-p = malloc(sizeof(dog_t));
-if (p == NULL)
-
+new = malloc(sizeof(dog_t));
+if (new == NULL)
 return (NULL);
-if (name == NULL)
+
+ncpy = _strdup(name);
+if (!ncpy && name)
 {
 
-free(p);
-free(owner);
+free(new);
 return (NULL);
 
 }
 
-if (owner == NULL)
+ocpy = _strdup(owner);
+if (!ocpy && owner)
 {
-
-free(p);
-free(name);
+free(ncpy);
+free(new);
 return (NULL);
-
 }
+new->name = ncpy;
+new->age = age;
+new->owner = ocpy;
 
-p->name = name;
-p->age = age;
-p->owner = owner;
-return (p);
-
+return (new);
 }
