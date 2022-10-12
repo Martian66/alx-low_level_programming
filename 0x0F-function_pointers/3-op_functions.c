2382048503d7cea9fcde30eaf6989f2ccaecae3c
Martian66 <kimmartin049@gmail.com>
2022-10-12 12:03:26 -0700

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *op_add - addition operator
+ *@a: first int
+ *@b: second int
+ *
+ * Return: sum of the two ints
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ *op_sub - subtraction operator
+ *@a: first int
+ *@b: second int
+ *
+ *Return: subtraction of two ints
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product of two ints
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+
+}
+
+/**
+ *op_div - divide two ints
+ *@a: first int
+ *@b: second int
+ *
+ * Return: divide two ints
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+
+}
+
+/**
+ * op_mod - modulo operation
+ * @a: first int
+ * @b: second it
+ *
+ * Return:  modulus of two ints
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+
+}
