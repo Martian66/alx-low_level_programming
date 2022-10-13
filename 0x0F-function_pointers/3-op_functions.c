b7558d7768c94e542d0004a6ca536e58ee0621ad
Martian66 <kimmartin049@gmail.com>
2022-10-13 00:38:25 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -52,6 +52,13 @@ return (a * b);
 
 int op_div(int a, int b)
 {
+
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+
+}
 return (a / b);
 
 }
@@ -66,6 +73,12 @@ return (a / b);
 
 int op_mod(int a, int b)
 {
+
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 
 }
