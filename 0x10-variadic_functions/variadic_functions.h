6ac327a2792c7444d27b024498862aec269d98ca
Martian66 <kimmartin049@gmail.com>
2022-10-13 04:50:35 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,20 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
 
-int _putchar(char c);
+#include <stdarg.h>
+/**
+ *struct print - print type with corresponding print function
+ *@t: print type
+ *@f: print function
+ */
+typedef struct print
+{
+char *t;
+void (*f)(va_list);
+
+} print_t;
+
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
