b9feb82c3c8aa7451ec1ad7f68f42a39d94e13e5
Martian66 <kimmartin049@gmail.com>
2022-10-13 12:56:53 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,9 +15,9 @@ void print_all(const char * const format, ...)
 va_list valist;
 unsigned int i = 0, j, c = 0;
 char  *str;
-const char t_arg[] = "cifs"
+const char t_arg[] = "cifs";
 
-va_start(valist, format)
+va_start(valist, format);
 while (format && format[i])
 {
 j = 0;
