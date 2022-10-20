dae7f9255763e1abef603be780dc66307982a7c4
Martian66 <kimmartin049@gmail.com>
2022-10-20 03:36:47 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,4 +10,12 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif/* lists_h_ */
+typdef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+
+} list_t
+
+#endif
