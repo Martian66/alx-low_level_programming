badeb95c4fa83109d166cb9127a0e766f8a78d49
Martian66 <kimmartin049@gmail.com>
2022-10-20 03:49:43 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,12 +3,6 @@
 
 #include <stdlib.h>
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 typdef struct list_s
 {
 char *str;
@@ -16,5 +10,11 @@ unsigned int len;
 struct list_s *next;
 
 } list_t
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
