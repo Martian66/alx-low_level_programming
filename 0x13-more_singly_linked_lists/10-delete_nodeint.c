0b35e37c118ad6d61bb63004af2a3d15c9321490
Martian66 <kimmartin049@gmail.com>
2022-10-25 02:50:16 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (copy == NULL)
 		return (-1);
-	if (index == NULL)
+
+	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(copy);
