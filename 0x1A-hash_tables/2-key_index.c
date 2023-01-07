15705cb3c0fc4ead1bf57af3de9f832df4b73754
Martian66 <kimmartin049@gmail.com>
2023-01-07 10:42:20 -0800

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,13 @@
 #include "hash_tables_h"
+
 /**
  * key_index - sets a hash table
  * @key: the key
  * @size: the value for size
+ *
  * Return: The index of the key
+ *
+ * Description: Uses the djb2 algorithm.
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
