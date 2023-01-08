30f265cb9865586e6384378ef8bc34edd86a8934
Martian66 <kimmartin049@gmail.com>
2023-01-07 23:06:01 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
+	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
@@ -16,10 +17,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	(*table).size = size;
 	(*table).array = malloc(sizeof(hash_node_t *) * size);
-	if (!(*table).array)
-	{
-		free(table);
+
+	if (table->array == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
 	}
 	return (table);
 }
