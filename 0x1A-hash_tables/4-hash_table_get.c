b78d421870722e233ff9896fc0ff2a94cc8bdb49
Martian66 <kimmartin049@gmail.com>
2023-01-08 09:53:59 -0800

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+* hash_table_get - gets a hash table
+* @ht: A pointer to the hash table.
+* @key: the key used to get the value
+*
+* Return: Value, NULL if key not found
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+	hash_node_t *ptr = NULL;
+	unsigned int index;
+
+	if (ht && key)
+	{
+		index = key_index((unsigned char *)key, ht->size);
+		ptr = ht->array[index];
+		if (ptr == NULL)
+			return (NULL);
+		while (strcmp(ptr->key, key) != 0)
+			ptr = ptr->next;
+		return (ptr->value);
+	}
+	return (NULL);
+}
